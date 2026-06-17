58977c23d57e778c5f92a506e9c9103a4d757605
Princebca <bishwakarmaprince081@gmail.com>
2026-06-17 10:20:54 +0530

    Fixing Assests

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -5,10 +5,7 @@ import help_icon from './help_icon.png'
 import logo_big from './logo-big.png'
 import logo from './logo.png'
 
-import profile_richard from './profile_richard.png'
-import profile_alison from './profile_alison.png'
-import profile_enrique from './profile_enrique.png'
-import profile_marco from './profile_marco.png'
+
 import profile_martin from './profile_martin.png'
 
 import search_icon from './search_icon.png'
@@ -17,15 +14,8 @@ import menu_icon from './menu_icon.png'
 import arrow_icon from './arrow_icon.png'
 
 import code from './code.svg'
-import bgImage from './bgImage.svg'
+import bgImage from '../../public/bgImage.svg'
 
-import pic1 from './pic1.png'
-import pic2 from './pic2.png'
-import pic3 from './pic3.png'
-import pic4 from './pic4.png'
-
-import img1 from './img1.jpg'
-import img2 from './img2.jpg'
 
 const assets = {
     avatar_icon,
@@ -42,19 +32,8 @@ const assets = {
 
     code,
     bgImage,
+    bgImage1,
 
-    pic1,
-    pic2,
-    pic3,
-    pic4,
-
-    img1,
-    img2,
-
-    profile_richard,
-    profile_alison,
-    profile_enrique,
-    profile_marco,
     profile_martin
 }
 
