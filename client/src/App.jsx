5b38ba2281b82cfe12912b30d7f98ae8959c6468
Princebca <bishwakarmaprince081@gmail.com>
2026-06-17 10:13:53 +0530

    Fixing Assests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,16 @@ import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import { Toaster } from 'react-hot-toast';
 import { AuthContext } from '../context/AuthContext'
+import bgImage from "./assets/bgImage.svg";
 
 const App = () => {
   const { authUser } = useContext(AuthContext)
 
   return (
-    <div className="bg-[url('/bgImage.svg)'] bg-cover text-white">
+    <div
+      className="bg-cover text-white"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <Toaster />
       <Routes>
         <Route
